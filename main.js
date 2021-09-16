494a4629d903b919088d3e1f73acd74139f73890
unicorastha <85661393+unicorastha@users.noreply.github.com>
2021-09-16 19:47:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+
+}
+function setup(){
+    canvas= createCanvas(640,480);
+    canvas.position(110,250);
+    video=creatorCapture(VIDEO);
+    video.hide();
+
+
+    tint_color ="" ;
+}
+
+function draw(){
+    Image(video, 0 ,0, 640,480)
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('student_name.png')
+}
+
+function filter_tint()
+{
+    tint_color=document.getElementById("color_name").value;
+}
